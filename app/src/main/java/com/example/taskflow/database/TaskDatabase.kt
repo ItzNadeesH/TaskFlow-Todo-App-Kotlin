ffac15dcc38573bfa64597f9abcbc0ab4f104b14
NadeesH <spnadeeshnimsara@gmail.com>
2024-05-11 22:56:56 +0530

    Implement task database

--- app/src/main/java/com/example/taskflow/database/TaskDatabase.kt
+++ app/src/main/java/com/example/taskflow/database/TaskDatabase.kt
@@ -1,4 +1,28 @@
 package com.example.taskflow.database
 
-abstract class TaskDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.taskflow.database.daos.TaskDao
+import com.example.taskflow.database.entities.Task
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase: RoomDatabase() {
+    abstract fun getTaskDao(): TaskDao
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDatabase? = null
+        fun getInstance(context: Context): TaskDatabase {
+            synchronized(this) {
+                return INSTANCE?: Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_db"
+                ).build().also {
+                    INSTANCE = it
+                }
+            }
+        }
+    }
 }
