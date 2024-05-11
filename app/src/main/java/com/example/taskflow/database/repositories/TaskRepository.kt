0adefc931222fe1742325c0cf4eb5e3d50fa752e
NadeesH <spnadeeshnimsara@gmail.com>
2024-05-11 23:00:46 +0530

    Implement task repositories

--- app/src/main/java/com/example/taskflow/database/repositories/TaskRepository.kt
+++ app/src/main/java/com/example/taskflow/database/repositories/TaskRepository.kt
@@ -1,4 +1,11 @@
 package com.example.taskflow.database.repositories
 
-class TaskRepository {
+import com.example.taskflow.database.TaskDatabase
+import com.example.taskflow.database.entities.Task
+
+class TaskRepository(private val db: TaskDatabase) {
+    suspend fun insert(task: Task) = db.getTaskDao().insertTask(task)
+    suspend fun update(task: Task) = db.getTaskDao().updateTask(task)
+    suspend fun delete(task: Task) = db.getTaskDao().deleteTask(task)
+    fun getTasks(): List<Task> = db.getTaskDao().getAllTasks()
 }
