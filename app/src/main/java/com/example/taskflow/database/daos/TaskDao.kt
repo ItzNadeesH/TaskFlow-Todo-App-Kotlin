70e518d010876690cb41b9399fee407c66c9ba34
NadeesH <spnadeeshnimsara@gmail.com>
2024-05-11 22:49:00 +0530

    Implement task data access object

--- app/src/main/java/com/example/taskflow/database/daos/TaskDao.kt
+++ app/src/main/java/com/example/taskflow/database/daos/TaskDao.kt
@@ -1,4 +1,19 @@
 package com.example.taskflow.database.daos
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.taskflow.database.entities.Task
+@Dao
 interface TaskDao {
+    @Insert
+    suspend fun insertTask(task: Task)
+    @Update
+    suspend fun updateTask(task: Task)
+    @Delete
+    suspend fun deleteTask(task: Task)
+    @Query("SELECT * FROM Task")
+    fun getAllTasks(): List<Task>
 }
