e2324dc9e1f3ecb4ddd403afc1a58f0a0810df98
NadeesH <spnadeeshnimsara@gmail.com>
2024-05-13 00:31:39 +0530

    Implement checkbox function

--- app/src/main/java/com/example/taskflow/adapters/TaskAdapter.kt
+++ app/src/main/java/com/example/taskflow/adapters/TaskAdapter.kt
@@ -8,6 +8,9 @@ import com.example.taskflow.R
 import com.example.taskflow.TaskViewHolder
 import com.example.taskflow.database.entities.Task
 import com.example.taskflow.database.repositories.TaskRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class TaskAdapter(private val data: List<Task>, private val repository: TaskRepository): RecyclerView.Adapter<TaskViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
@@ -23,8 +26,16 @@ class TaskAdapter(private val data: List<Task>, private val repository: TaskRepo
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val singleData = data[position]
         holder.taskName.text = singleData.taskName
-        if (singleData.status) {
-            holder.taskName.isChecked = true
+        holder.taskName.isChecked = singleData.status // Set the initial checked state
+
+        holder.taskName.setOnCheckedChangeListener { _, isChecked ->
+            // Update the status of the task in the database when the checkbox state changes
+            if (isChecked != singleData.status) {
+                singleData.status = isChecked
+                CoroutineScope(Dispatchers.IO).launch {
+                    repository.update(singleData)
+                }
+            }
         }
     }
 }
